b0ed669bc356bf919e1326be9df1a6dd1a97679a
nedoletoff <nedoletoff@yandex.ru>
2022-07-16 15:01:23 +0300

    доработаны уровни создан класс для сохранения уровня

--- Level.java
+++ Level.java
@@ -1,19 +1,27 @@
 import java.io.Serializable;
 import java.util.*;
 
-public class Level implements Serializable {
+public class Level {
     String levelName;
+    int timer;
+    int points = 0;
     TreeMap<Integer, GameObject> gameObjects;
+    public TreeSet<Integer> pressedKeys;
 
     public Level(String levelName) {
         this.levelName = levelName;
         this.gameObjects = new TreeMap<>();
+        pressedKeys = new TreeSet<>();
     }
 
     public void addGameObject(GameObject gameObject) {
         Integer id = gameObject.getId();
         if (!gameObjects.containsKey(id))
             gameObjects.put(id, gameObject);
+    }
+
+    public void removeGameObject(Integer id) {
+        gameObjects.remove(id);
     }
 
     public void setLevelName(String levelName) {
@@ -28,7 +36,7 @@ public class Level implements Serializable {
         return gameObjects.size();
     }
 
-    public GameObject getGameObjects(int id) {
+    public GameObject getGameObject(Integer id) {
         return gameObjects.get(id);
     }
 
@@ -38,6 +46,45 @@ public class Level implements Serializable {
         while (it.hasNext())
             arrayList.add(it.next());
         return arrayList;
+    }
+
+    public TreeMap<Integer, GameObject> getGameObjects() {
+        return gameObjects;
+    }
+    public int getPoints() {
+        return points;
+    }
+
+    public int getTimer() {
+        return timer;
+    }
+
+    public void setTimer(int timer) {
+        this.timer = timer;
+    }
+
+    public void addPoint() {
+        points++;
+    }
+
+    public static Level load(String filename) {
+        SavedLevel sl = SavedLevel.load(filename);
+        Level l = new Level(sl.levelName);
+        l.gameObjects = sl.gameObjects;
+        return l;
+    }
+
+    public void save(String filename) {
+        String path = "C:\\Users\\nedoletoff\\IdeaProjects\\game\\levels\\";
+        SavedLevel sl = SavedLevel.load(path + levelName);
+        sl.bestPoints = Math.max(sl.bestPoints, getPoints());
+        sl.bestTime = Math.min(sl.bestTime, getTimer());
+    }
+
+    public void redactorSave() {
+        String path = "C:\\Users\\nedoletoff\\IdeaProjects\\game\\levels\\";
+        SavedLevel sl = new SavedLevel(this);
+        sl.save(path + levelName);
     }
 }
 
---
+++ SavedLevel.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.TreeMap;
+
+public class SavedLevel implements Serializable {
+    public int bestTime = 9999;
+    public int bestPoints = 0;
+    public TreeMap<Integer, GameObject> gameObjects;
+    public String levelName;
+
+    public SavedLevel(Level level) {
+        gameObjects = level.getGameObjects();
+        levelName = level.getLevelName();
+    }
+
+    public SavedLevel(SavedLevel other) {
+        gameObjects = other.gameObjects;
+        levelName = other.levelName;
+        bestPoints = other.bestPoints;
+        bestTime = other.bestTime;
+    }
+
+    public static SavedLevel load(String filename) {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
+            return (SavedLevel) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void save(String filename) {
+        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename))) {
+            oos.writeObject(this);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
