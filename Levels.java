1081db40e54f1747d030db87f206dd65f4ff824f
nedoletoff <nedoletoff@yandex.ru>
2022-07-19 18:22:08 +0300

    создан класс для работы с существующими уровнями

---
+++ Levels.java
@@ -0,0 +1,38 @@
+import java.io.File;
+import java.util.ArrayList;
+
+public class Levels {
+    ArrayList<String> filesName;
+    File path;
+
+    public Levels(String path) {
+        filesName = new ArrayList<>();
+        this.path = new File(path);
+        File[] files = this.path.listFiles();
+        assert files != null;
+        for (File file : files) {
+            filesName.add(file.getName());
+        }
+
+    }
+
+    public void setPath(File path) {
+        this.path = path;
+    }
+
+    public ArrayList<String> getFilesName() {
+        return filesName;
+    }
+
+    public ArrayList<String> getLevelsNames() {
+        ArrayList<String> res = new ArrayList<>();
+        for (String name : filesName) {
+            res.add(name.split("\\.")[0]);
+        }
+        return res;
+    }
+
+    public File getPath() {
+        return path;
+    }
+}
--- SavedLevel.java
+++ SavedLevel.java
@@ -19,15 +19,18 @@ public class SavedLevel implements Serializable {
         bestTime = other.bestTime;
     }
 
-    public static SavedLevel load(String filename) {
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
+    public static SavedLevel load(String levelName) {
+        levelName += ".lvl";
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(levelName))) {
             return (SavedLevel) ois.readObject();
         } catch (IOException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
-    public void save(String filename) {
-        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename))) {
+    public void save(String levelName) {
+        levelName += ".lvl";
+        try(ObjectOutputStream oos = new ObjectOutputStream(new
+                FileOutputStream(levelName))) {
             oos.writeObject(this);
         } catch (IOException e) {
             throw new RuntimeException(e);
