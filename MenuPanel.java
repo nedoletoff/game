649c6f78f2690f62fb30cd74baf9e3829a19e78a
nedoletoff <nedoletoff@yandex.ru>
2022-07-19 05:36:38 +0300

    добавлено взаимодейсвие с меню, теперь можно выйти нажатием exit

--- MenuPanel.java
+++ MenuPanel.java
@@ -1,19 +1,46 @@
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
+import java.util.Objects;
 
 public class MenuPanel extends JPanel implements ActionListener {
     Menu menu = new Menu();
 
     public MenuPanel(Frame frame) {
+        MenuPanel menuPanel = this;
         JLabel jLabel = new JLabel(menu.getBack());
         frame.add(jLabel);
         jLabel.add(menu.getMenu());
         jLabel.add(menu.getGameName());
+        final int[] selected = new int[1];
+        menu.getMenu().addListSelectionListener(e -> selected[0] = ((JList<?>)e.getSource()).
+                getSelectedIndex());
+        menu.getMenu().addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    System.out.println("Pressed " + selected[0]);
+                    menuPanel.actionPerformed(new ActionEvent(e.getSource(),
+                            e.getID(), "Enter " + selected[0]));
+                }
+            }
+        });
+
+        menu.getMenu().addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 2) {
+                    System.out.println("Pressed " + selected[0]);
+                    menuPanel.actionPerformed(new ActionEvent(e.getSource(),
+                            e.getID(), "Enter " + selected[0]));
+                }
+            }
+        });
     }
     @Override
     public void actionPerformed(ActionEvent e) {
+        if (Objects.equals(e.getActionCommand(), "Enter 3"))
+            System.exit(0);
 
     }
 }
